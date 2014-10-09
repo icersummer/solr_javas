5eda9d4aabda6922b04a81ee56b5bbee245a6b18
icersummer <guofangsky@gmail.com>
2014-10-09 12:21:47 +0800

    the project can be remote debug

--- bat/startTomcat.bat
+++ bat/startTomcat.bat
@@ -4,6 +4,8 @@ set JRE_HOME=D:\ptc\FX24_latest_build\Java\jre
 set CLASSPATH=%JRE_HOME%\lib
 set PATH=%PATH%;%JRE_HOME%\bin
 set JAVA=java
+set CATALINA_OPTS=-server -Xnoagent -Xdebug -Xrunjdwp:transport=dt_socket,address=5888,server=y,suspend=n
 call %CATALINA_HOME%\bin\startup.bat 
+rem call %CATALINA_HOME%\bin\cataline start 
 
 echo 关闭的话，只是最后一句改成直接调shutdown.bat.
